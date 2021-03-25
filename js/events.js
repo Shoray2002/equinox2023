4bb176a287ab7dd77954acb47e9d16f26392c5ac
blank-27 <lcs2019045@iiitl.ac.in>
2021-03-25 15:33:34 +0530

    stock img added

--- js/events.js
+++ js/events.js
@@ -9,21 +9,21 @@ window.addEventListener('load',() => {
 // ///////////////////////EVENTS FUNC/////////////
 
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 const event_1 = document.querySelector('#event_1');//conclave Workshop
 const event_2 = document.querySelector('#event_2');//sponsors
 const event_3 = document.querySelector('#event_3');//home heading
 const event_4 = document.querySelector('#event_4');//about
 const event_5 = document.querySelector('#event_5');//events
 const event_6 = document.querySelector('#event_6');//team
-=======
-const event_1 = document.querySelector('#event_1>span');//workshop
-const event_2 = document.querySelector('#event_2>span');//sponsors
-const event_3 = document.querySelector('#event_3>span');//home heading
-const event_4 = document.querySelector('#event_4>span');//about
-const event_5 = document.querySelector('#event_5>a');//events
-const event_6 = document.querySelector('#event_6>span');//team
->>>>>>> b7d07e896bfb6f3d8823319b0b103e6f47e98684
+// =======
+// const event_1 = document.querySelector('#event_1>span');//workshop
+// const event_2 = document.querySelector('#event_2>span');//sponsors
+// const event_3 = document.querySelector('#event_3>span');//home heading
+// const event_4 = document.querySelector('#event_4>span');//about
+// const event_5 = document.querySelector('#event_5>a');//events
+// const event_6 = document.querySelector('#event_6>span');//team
+// >>>>>>> b7d07e896bfb6f3d8823319b0b103e6f47e98684
 
 const aboutHome = document.querySelector('#about__home');
 const teamHome = document.querySelector('#team__home');
