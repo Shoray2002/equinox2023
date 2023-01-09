daed4f1101f47dd2379836f6c703342f34d9a540
Shoray Singhal <shoryasinghall@gmail.com>
2023-01-10 00:56:38 +0530

    added preloader new

--- js/app.js
+++ js/app.js
@@ -275,16 +275,16 @@ var asciis = [
     "                `=--=-'    ",
   ],
   [
-    "       .--.                   .---.   ",
-    "   .---|__|           .-.     |~~~|   ",
-    ".--|===|--|_          |_|     |~~~|--.",
-    "|  |===|  |'     .---!~|  .--|   |--| ",
-    "|%%|   |  |.'    |===| |--|%%|   |  | ",
-    "|%%|   |  |.'   |   | |__|  |   |  |  ",
-    "|  |   |  |     |===| |==|  |   |  |  ",
-    "|  |   |__|  .' |   |_|__|  |~~~|__|  ",
-    "|  |===|--|   .'|===|~|--|%%|~~~|--|  ",
-    "^--^---'--^    `-'`---^-^--^--^---'--'",
+    "       .--.                     .---.  ",
+    "   .---|__|           .--.      |~~~|  ",
+    ".--|===|--|_          |__|      |~~~|  ",
+    "|  |===|  |'    .---!~|  |--|   |-- |  ",
+    "|%%|   |  |.'   |===| |--|%%|   |   |  ",
+    "|%%|   |  |.'   |   | |__|  |   |   |  ",
+    "|  |   |  |     |===| |==|  |   |   |  ",
+    "|  |   |__|  .' |   | |__|  |~~~|___|  ",
+    "|  |===|--|   .'|===|~|--|%%|~~~|---|  ",
+    "^--^---'--^    `-'`---^-^--^--^---'--' ",
   ],
 
   // [
@@ -315,15 +315,15 @@ var asciis = [
     "  /  _             /   /         /|",
     " /  ._.`          /   /    .   ./ |",
     "/________________/___/____|0000|  |",
-    "|   /|\\         |UUUU|    |0000| |",
-    "|  . | ,         `''`          | p|",
-    "|  | | |         .---.   o     |  |",
+    "|   /|\\          |UUUU|   |0000|  |",
+    "|  . | ,         `''`          |  |",
+    "|  | | |         .---.         |  |",
     "|  | | |       .` .o, `.       |  |",
     "|  | | |       -  OOO  -       |  |",
     "|  | | |       '  'o`  '       | /|",
-    "|  | | |       /'....-'       |/A|",
-    "|--' | '------|---------|------|A/|",
-    " ___|/_______|_________|______ /",
+    "|  | | |       /'....-'        |/ |",
+    "|--' | '------|---------|------| /|",
+    "|____|/_______|_________|_______/_|",
   ],
 
   [
@@ -425,6 +425,7 @@ const stopMorph = () => {
     window.clearInterval(i);
   }
 };
+
 const donutShow = () => {
   setInterval(() => {
     (z += 0.07), (y += 0.03);
@@ -459,7 +460,6 @@ const donutShow = () => {
     donutElement.innerHTML = a.join("");
   }, 50);
 };
-
 const stopDonut = () => {
   donutElement.innerHTML = "";
   const interval_id = window.setInterval(function () {},
@@ -468,183 +468,3 @@ const stopDonut = () => {
     window.clearInterval(i);
   }
 };
-
-//////////////////BOT//////////
-// const event_6 = document.querySelector('#event_6');
-// const event6 = document.querySelector('#home__container');
-// const bot = document.querySelector('#bot');
-
-// var bot = document.querySelector("#bot");
-// var container = document.querySelector("html");
-
-// container.addEventListener("click", function(event) {
-// 	var xPosition = event.clientX - container.getBoundingClientRect().left - (bot.clientWidth / 2);
-// 	var yPosition = event.clientY - container.getBoundingClientRect().top - (bot.clientHeight / 2);
-// 	// in case of a wide border, the boarder-width needs to be considered in the formula above
-// 	bot.style.left = xPosition + "px";
-// 	bot.style.top = yPosition + "px";
-// 	}
-// );
-
-// https://greensock.com/forums/topic/26113-positioning-an-element-on-scroll-relative-to-another-scrolltrigger/
-
-// const change = () => {
-//     console.log("hovered");
-//     document.getElementById("canvas-container").style.opacity = 1;
-//     const func = () => {
-//       const scene = new THREE.Scene();
-//       const renderer = new THREE.WebGLRenderer();
-//       renderer.setSize(window.innerWidth, window.innerHeight);
-
-//       document
-//         .getElementById("canvas-container")
-//         .appendChild(renderer.domElement);
-
-//       const geometry = new THREE.SphereGeometry(1, 640, 64);
-//       const textureLoader = new THREE.TextureLoader();
-//       const normalTexture = textureLoader.load(
-//         "https://i.ibb.co/4gHcRZD/bg3-je3ddz.jpg",
-//         function () {
-//           console.log("loaded");
-//           document.getElementById("loader").style.display = "none";
-//         }
-//       );
-
-//       const loader = new THREE.TextureLoader();
-//       // scene.background = loader.load( 'https://i.ibb.co/4gHcRZD/bg3-je3ddz.jpg' );
-
-//       const texture = new THREE.TextureLoader().load(
-//         "https://i.ibb.co/4gHcRZD/bg3-je3ddz.jpg"
-//       );
-//       const material = new THREE.PointsMaterial({
-//         size: 0.01,
-//         color: "#FFFFFF",
-//       });
-
-//       const material3 = new THREE.MeshStandardMaterial();
-//       const material2 = new THREE.PointsMaterial({
-//         size: 0.001,
-//         color: "#FFFFFF",
-//       });
-//       material3.metalness = 0.15;
-//       material3.roughness = 0.95;
-//       material3.normalMap = normalTexture;
-//       material3.map = texture;
-//       material3.color = new THREE.Color(0xffffff);
-//       material2.metalness = 0.15;
-//       material2.roughness = 0.95;
-//       material2.normalMap = normalTexture;
-//       material2.map = texture;
-//       material2.color = new THREE.Color(0xffffff);
-
-//       const particlesGeometry = new THREE.BufferGeometry();
-//       const particlesGeometry2 = new THREE.BufferGeometry();
-//       const particleCnt = 500;
-//       const posArray = new Float32Array(particleCnt * 3);
-//       const posArray2 = new Float32Array(particleCnt * 3);
-
-//       for (let i = 0; i < particleCnt * 3; i++) {
-//         posArray[i] = (Math.random() - 0.5) * 5;
-//       }
-//       for (let i = 0; i < particleCnt * 3; i++) {
-//         posArray2[i] = (Math.random() - 0.5) * 5;
-//       }
-//       particlesGeometry.setAttribute(
-//         "position",
-//         new THREE.BufferAttribute(posArray, 3)
-//       );
-//       particlesGeometry2.setAttribute(
-//         "position",
-//         new THREE.BufferAttribute(posArray2, 3)
-//       );
-
-//       {
-//         let loader = new THREE.TextureLoader(),
-//           texture = loader.load("https://i.ibb.co/4gHcRZD/bg3-je3ddz.jpg");
-
-//         texture.anisotropy = 20;
-
-//         let geometry = new THREE.SphereBufferGeometry(200, 60, 60),
-//           material = new THREE.MeshBasicMaterial({
-//             side: THREE.BackSide,
-//             map: texture,
-//           });
-
-//         globe = new THREE.Mesh(geometry, material);
-//         scene.add(globe);
-//       }
-
-//       const sphere = new THREE.Mesh(geometry, material3);
-//       const particlesMesh = new THREE.Points(particlesGeometry, material);
-//       const particlesMesh2 = new THREE.Points(particlesGeometry2, material2);
-//       scene.add(sphere);
-
-//       let ambientLight = new THREE.AmbientLight("#ffffff", 0.4);
-//       ambientLight.position.set(0, 20, 20);
-//       scene.add(ambientLight);
-
-//       let mouseX = 0;
-//       let mouseY = 0;
-//       document.addEventListener("mousemove", (Event) => {
-//         mouseX = Event.clientX;
-//         mouseY = Event.clientY;
-//       });
-//       const pointLight = new THREE.PointLight(0xffffff, 0.3);
-//       pointLight.position.set(40.5, -77.6, 21);
-//       scene.add(pointLight);
-
-//       const pointLight2 = new THREE.PointLight(0xffffff, 0.8);
-//       pointLight2.position.set(-2.5, 0.7, 2.9);
-//       scene.add(pointLight2);
-
-//       const sizes = {
-//         width: window.innerWidth,
-//         height: window.innerHeight,
-//       };
-
-//       window.addEventListener("resize", () => {
-//         sizes.width = window.innerWidth;
-//         sizes.height = window.innerHeight;
-//         camera.aspect = sizes.width / sizes.height;
-//         camera.updateProjectionMatrix();
-//         renderer.setSize(sizes.width, sizes.height);
-//         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-//       });
-
-//       const camera = new THREE.PerspectiveCamera(
-//         75,
-//         sizes.width / sizes.height,
-//         0.1,
-//         100
-//       );
-//       camera.position.x = 0;
-//       camera.position.y = 0;
-//       camera.position.z = 2;
-//       scene.add(camera);
-
-//       camera.position.z = 3;
-//       const clock = new THREE.Clock();
-
-//       const tick = () => {
-//         const elapsedTime = clock.getElapsedTime();
-//         particlesMesh.rotation.y = 0.5 * elapsedTime;
-//         sphere.rotation.y = 0.5 * elapsedTime;
-//         if (mouseX > 0) {
-//           particlesMesh.position.x = -mouseY * (elapsedTime * 0.000008);
-//           particlesMesh.position.y = -mouseX * (elapsedTime * 0.000008);
-//         }
-//         particlesMesh2.rotation.y = 1 * elapsedTime;
-
-//         renderer.render(scene, camera);
-
-//         window.requestAnimationFrame(tick);
-//       };
-
-//       tick();
-//     };
-//     func();
-//   };
-//   const changeagain = () => {
-//     console.log("leave");
-//     document.getElementById("canvas-container").style.opacity = 0;
-//   };
--- js/events.js
+++ js/events.js
@@ -3,57 +3,34 @@
 const timeCount = 3000;
 
 const counting = () => {
-    for (let i = 1; i <= timeCount/1000; i++) {
-        setTimeout(() => {
-        console.log(i);
-      }, i * 1000);
-    }
-}
+  for (let i = 1; i <= timeCount / 1000; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+};
 
 counting();
 
 setTimeout(() => {
-    const preload = document.querySelector('.preloader');
-    preload.classList.add('preloader-finish');
-    console.log("loaded")
-},timeCount);
-
-
-// const preload = document.querySelector('.preloader');
-
-// if (document.readyState === 'interactive') {
-// }
-
-// document.onreadystatechange = function () {
-//     if (document.readyState === 'complete') {
-//         preload.classList.add('preloader-finish');
-//     }
-// }
-
-
-// $(document).ready(function() {
-//     setTimeout(function() {
-//         $('preloader').addClass('preloader-finish');
-//     }, 1500);
-// });
-
-
-
+  const preload = document.querySelector(".preloader");
+  preload.classList.add("preloader-finish");
+  console.log("loaded");
+}, timeCount);
 
 // ///////////////////////EVENTS FUNC/////////////
 
-
 // <<<<<<< HEAD
-const event_1 = document.querySelector('#event_1>a');//conclave Workshop
-const event_2 = document.querySelector('#event_2>span');//sponsors
-const event_3 = document.querySelector('#event_3>span');//home heading
-const event_4 = document.querySelector('#event_4>span');//about
-const event_5 = document.querySelector('#event_5>a');//events
-const event_6 = document.querySelector('#event_6>span');//team
+const event_1 = document.querySelector("#event_1>a"); //conclave Workshop
+const event_2 = document.querySelector("#event_2>span"); //sponsors
+const event_3 = document.querySelector("#event_3>span"); //home heading
+const event_4 = document.querySelector("#event_4>span"); //about
+const event_5 = document.querySelector("#event_5>a"); //events
+const event_6 = document.querySelector("#event_6>span"); //team
 
-const event2_1 = document.querySelector('#event2_1>span');//conclave Workshop
-const event2_2 = document.querySelector('#event2_2>span');//sponsors
-const event2_3 = document.querySelector('#event2_3>span');//home heading
+const event2_1 = document.querySelector("#event2_1>span"); //conclave Workshop
+const event2_2 = document.querySelector("#event2_2>span"); //sponsors
+const event2_3 = document.querySelector("#event2_3>span"); //home heading
 // =======
 // const event_1 = document.querySelector('#event_1>span');//workshop
 // const event_2 = document.querySelector('#event_2>span');//sponsors
@@ -63,246 +40,216 @@ const event2_3 = document.querySelector('#event2_3>span');//home heading
 // const event_6 = document.querySelector('#event_6>span');//team
 // >>>>>>> b7d07e896bfb6f3d8823319b0b103e6f47e98684
 
-const aboutHome = document.querySelector('#about__home');
-const teamHome = document.querySelector('#team__home');
-const sponsorsHome = document.querySelector('#sponsors__home');
-const workshopHome = document.querySelector('#workshop__home');
+const aboutHome = document.querySelector("#about__home");
+const teamHome = document.querySelector("#team__home");
+const sponsorsHome = document.querySelector("#sponsors__home");
+const workshopHome = document.querySelector("#workshop__home");
 
-
-
-let yheight,xwidth;
+let yheight, xwidth;
 // let events = [event_1,event_2,event_3,event_4,event_5,event_6]
 
 ////////////////////functionality section///////
 
-
-
 //about
-var  e1 = gsap.timeline({defaults:{duration: 0.4,}},{paused:true}) 
+var e1 = gsap.timeline({ defaults: { duration: 0.4 } }, { paused: true });
 // sponsors
-var  e2 = gsap.timeline({defaults:{duration: 0.4}},{paused:true}) 
+var e2 = gsap.timeline({ defaults: { duration: 0.4 } }, { paused: true });
 
 // Team
-var  e3 = gsap.timeline({defaults:{duration: 0.4}},{paused:true}) 
+var e3 = gsap.timeline({ defaults: { duration: 0.4 } }, { paused: true });
 
 //conclave Workshop
-var  e4 = gsap.timeline({defaults:{duration: 0.4}},{paused:true}) 
-
-
-
+var e4 = gsap.timeline({ defaults: { duration: 0.4 } }, { paused: true });
 
 const fn_event_1 = () => {
+  e1.to("#countdown", { duration: 0.01, opacity: 0 })
+    .to("footer", { duration: 0.01, opacity: 0 })
+    .to(".nav__category,.home__container__img", { duration: 0.02, opacity: 0 })
+    .to(".countdown", { duration: 0.01, opacity: 0 })
+    .to(".about--section", { scale: 1 })
+    .to(".about--section", { height: "100vh" })
+    .to(".about--section", { duration: 1, width: "100vw", ease: "power4.out" })
+    .to(".background__line", { height: "10vh", width: "10vw" })
+    .to(".background__line", { height: "100vh" })
+    .to(".background__back", { duration: 0.01, opacity: 1 })
+    .to(".about__heading,.about__home", { duration: 0.01, opacity: 1 })
+    .to(".about__heading--bg", { opacity: 1 })
+    .to(".about__images", { scale: 1, opacity: 1 })
+    .to(".about__details", { scale: 1, opacity: 1 }, "-=1");
+};
 
-    
-    e1.to("#countdown",{duration:0.01, opacity:0})
-    .to("footer",{duration:0.01, opacity:0})
-    .to(".nav__category,.home__container__img",{duration:0.02, opacity:0})
-    .to(".countdown",{duration:0.01, opacity:0})
-    .to(".about--section",{scale:1})
-    .to(".about--section",{height:"100vh"})
-    .to(".about--section",{duration: 1,width:"100vw",ease:"power4.out"})
-    .to(".background__line",{height:"10vh",width:"10vw"})
-    .to(".background__line",{height:"100vh"})
-    .to(".background__back",{duration:0.01, opacity:1})
-    .to(".about__heading,.about__home",{duration:0.01, opacity:1})
-    .to(".about__heading--bg",{opacity:1})
-    .to(".about__images",{scale:1,opacity:1})
-    .to(".about__details",{scale:1,opacity:1},"-=1")
-
-}
-
-
-
-const fn_event_2 = () => {                
-        e2.to("#countdown",{duration:0.01, opacity:0})
-        .to("footer",{duration:0.01, opacity:0})
-        .to(".nav__category,.home__container__img",{duration:0.02, opacity:0})
-        .to(".countdown",{duration:0.01, opacity:0})
-        .to(".sponsors--section",{scale:1},"-=1")
-        .to(".sponsors--section",{duration:0.1,height:"100vh"},"-=1")
-        .to(".sponsors--section",{duration: 0,width:"100vw",ease:"power4.out"})
-        .to(".background__line",{width:"10vw"},"-=1")
-        // .to(".background__line",{height:"100vh"})    
-        // .to(".background__line",{height:"0vh",width:"10vw"})
-        .to(".background__line--1",{duration: 0.4,height:"100vh"})
-        .to(".background__line--2",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--3",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--4",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--5",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--6",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--7",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--8",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--9",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__line--10",{duration: 0.4,height:"100vh"},"-=0.30")
-        .to(".background__back",{duration:0.01, opacity:1})
-        .to(".sponsors__heading--bg",{opacity:1})
-        .to(".sponsors__heading",{duration:0.01, opacity:1})
-        .to(".sponsors__row",{duration:0.03,opacity:1})
-        .to(".sponsors__footer",{duration:0.0,opacity:1},"-=1")
-}
-
-
-
-
-const fn_event_3 = () => {                
-    e3.to("#countdown",{duration:0.01, opacity:0})
-    .to("footer",{duration:0.01, opacity:0})
-    .to(".nav__category,.home__container__img",{duration:0.02, opacity:0})
-    .to(".countdown",{duration:0.01, opacity:0})
-    .to(".team--section",{scale:1})
-    .to(".team--section",{height:"100vh"})
-    .to(".team--section",{duration: 1,width:"100vw",ease:"power4.out"})
-    .to(".background__line",{height:"10vh",width:"10vw"})
-    .to(".background__line",{height:"100vh"})
-    .to(".background__back",{duration:0.01, opacity:1})
-    .to(".team__heading--bg",{opacity:1})
-    .to(".team__heading",{duration:0.01, opacity:1})
-    .to(".team__card",{duration:0.01,opacity:1})
-    // .to(".sponsors__footer",{duration:0.01,opacity:1},"-=1")
-}
-
-
-const fn_event_4 = () => {                
-    e4.to("#countdown",{duration:0.01, opacity:0})
-    .to("footer",{duration:0.01, opacity:0})
-    .to(".nav__category,.home__container__img",{duration:0.02, opacity:0})
-    .to(".countdown",{duration:0.01, opacity:0})
-    .to(".workshop--section",{scale:1},"-=1")
-    .to(".workshop--section",{duration:0.1,height:"100vh"},"-=1")
-    .to(".workshop--section",{duration: 0,width:"100vw",ease:"power4.out"})
-    .to(".background__line",{width:"10vw"},"-=1")
-    // .to(".background__line",{height:"100vh"})    
+const fn_event_2 = () => {
+  e2.to("#countdown", { duration: 0.01, opacity: 0 })
+    .to("footer", { duration: 0.01, opacity: 0 })
+    .to(".nav__category,.home__container__img", { duration: 0.02, opacity: 0 })
+    .to(".countdown", { duration: 0.01, opacity: 0 })
+    .to(".sponsors--section", { scale: 1 }, "-=1")
+    .to(".sponsors--section", { duration: 0.1, height: "100vh" }, "-=1")
+    .to(".sponsors--section", {
+      duration: 0,
+      width: "100vw",
+      ease: "power4.out",
+    })
+    .to(".background__line", { width: "10vw" }, "-=1")
+    // .to(".background__line",{height:"100vh"})
     // .to(".background__line",{height:"0vh",width:"10vw"})
-    .to(".background__line--1",{duration: 0.4,height:"100vh"})
-    .to(".background__line--2",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--3",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--4",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--5",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--6",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--7",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--8",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--9",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__line--10",{duration: 0.4,height:"100vh"},"-=0.30")
-    .to(".background__back",{duration:0.01, opacity:1})
-    .to(".workshop__container",{duration:0.01, opacity:1})
-    .to(".workshop__heading",{ duration:0.3,scale:1,opacity:1})
+    .to(".background__line--1", { duration: 0.4, height: "100vh" })
+    .to(".background__line--2", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--3", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--4", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--5", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--6", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--7", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--8", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--9", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--10", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__back", { duration: 0.01, opacity: 1 })
+    .to(".sponsors__heading--bg", { opacity: 1 })
+    .to(".sponsors__heading", { duration: 0.01, opacity: 1 })
+    .to(".sponsors__row", { duration: 0.03, opacity: 1 })
+    .to(".sponsors__footer", { duration: 0.0, opacity: 1 }, "-=1");
+};
 
-    // .to(".sponsors__row",{duration:0.03,opacity:1})
-    // .to(".sponsors__footer",{duration:0.0,opacity:1},"-=1")
-}
+const fn_event_3 = () => {
+  e3.to("#countdown", { duration: 0.01, opacity: 0 })
+    .to("footer", { duration: 0.01, opacity: 0 })
+    .to(".nav__category,.home__container__img", { duration: 0.02, opacity: 0 })
+    .to(".countdown", { duration: 0.01, opacity: 0 })
+    .to(".team--section", { scale: 1 })
+    .to(".team--section", { height: "100vh" })
+    .to(".team--section", { duration: 1, width: "100vw", ease: "power4.out" })
+    .to(".background__line", { height: "10vh", width: "10vw" })
+    .to(".background__line", { height: "100vh" })
+    .to(".background__back", { duration: 0.01, opacity: 1 })
+    .to(".team__heading--bg", { opacity: 1 })
+    .to(".team__heading", { duration: 0.01, opacity: 1 })
+    .to(".team__card", { duration: 0.01, opacity: 1 });
+  // .to(".sponsors__footer",{duration:0.01,opacity:1},"-=1")
+};
 
-    
-  
-event_1.addEventListener('click',(e) => {
-    fn_event_4();
-    e4.play();
-})
-event2_1.addEventListener('click',(e) => {
-    fn_event_4();
-    e4.play();
-})
-event_2.addEventListener('click',(e) => {
-    fn_event_2();
-    e2.play();
-})
-event2_2.addEventListener('click',(e) => {
-    fn_event_2();
-    e2.play();
-})
-event_4.addEventListener('click',(e) => {
-    fn_event_1();
-    e1.play();
-    console.log("as");
-})
-event2_4.addEventListener('click',(e) => {
-    fn_event_1();
-    e1.play();
-    console.log("as");
-})
+const fn_event_4 = () => {
+  e4.to("#countdown", { duration: 0.01, opacity: 0 })
+    .to("footer", { duration: 0.01, opacity: 0 })
+    .to(".nav__category,.home__container__img", { duration: 0.02, opacity: 0 })
+    .to(".countdown", { duration: 0.01, opacity: 0 })
+    .to(".workshop--section", { scale: 1 }, "-=1")
+    .to(".workshop--section", { duration: 0.1, height: "100vh" }, "-=1")
+    .to(".workshop--section", {
+      duration: 0,
+      width: "100vw",
+      ease: "power4.out",
+    })
+    .to(".background__line", { width: "10vw" }, "-=1")
+    // .to(".background__line",{height:"100vh"})
+    // .to(".background__line",{height:"0vh",width:"10vw"})
+    .to(".background__line--1", { duration: 0.4, height: "100vh" })
+    .to(".background__line--2", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--3", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--4", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--5", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--6", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--7", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--8", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--9", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__line--10", { duration: 0.4, height: "100vh" }, "-=0.30")
+    .to(".background__back", { duration: 0.01, opacity: 1 })
+    .to(".workshop__container", { duration: 0.01, opacity: 1 })
+    .to(".workshop__heading", { duration: 0.3, scale: 1, opacity: 1 });
 
+  // .to(".sponsors__row",{duration:0.03,opacity:1})
+  // .to(".sponsors__footer",{duration:0.0,opacity:1},"-=1")
+};
 
+event_1.addEventListener("click", (e) => {
+  fn_event_4();
+  e4.play();
+});
+event2_1.addEventListener("click", (e) => {
+  fn_event_4();
+  e4.play();
+});
+event_2.addEventListener("click", (e) => {
+  fn_event_2();
+  e2.play();
+});
+event2_2.addEventListener("click", (e) => {
+  fn_event_2();
+  e2.play();
+});
+event_4.addEventListener("click", (e) => {
+  fn_event_1();
+  e1.play();
+  console.log("as");
+});
+event2_4.addEventListener("click", (e) => {
+  fn_event_1();
+  e1.play();
+  console.log("as");
+});
 
-event_6.addEventListener('click',(e) => {
+event_6.addEventListener("click", (e) => {
+  console.log("clicked");
 
-    console.log("clicked");
+  // e1.play();
 
-    // e1.play();
+  fn_event_3();
 
-    fn_event_3();
+  e3.play();
 
-    e3.play();
+  console.log("as");
+});
 
-    console.log("as");
-})
+aboutHome.addEventListener("click", () => {
+  console.log("as");
+  e1.reversed() ? e1.play() : e1.reverse();
+});
 
+sponsorsHome.addEventListener("click", () => {
+  console.log("clicked back spon");
 
+  e2.reversed() ? e2.play() : e2.reverse();
+});
 
+teamHome.addEventListener("click", () => {
+  console.log("vackteam");
+  e3.reversed() ? e3.play() : e3.reverse();
+});
 
-aboutHome.addEventListener('click',()=>{
-    
-    console.log("as");
-    e1.reversed() ? e1.play() : e1.reverse();
-})
+workshopHome.addEventListener("click", () => {
+  console.log("clicked back concla");
+  window.location.hash = "";
 
-sponsorsHome.addEventListener('click',()=>{  
-    console.log("clicked back spon");
-
-    e2.reversed() ? e2.play() : e2.reverse();
-})
-
-teamHome.addEventListener('click',()=>{
-    console.log("vackteam");
-    e3.reversed() ? e3.play() : e3.reverse();
-})
-
-workshopHome.addEventListener('click',()=>{
-
-    console.log("clicked back concla");
-    window.location.hash = "";
-
-    e4.reversed() ? e4.play() : e4.reverse();
-})
+  e4.reversed() ? e4.play() : e4.reverse();
+});
 
 const callConclave = () => {
-    if(window.location.hash =="#conclave"){
-        fn_event_4();
-        e4.play();        
-        console.log("exec")
-    }
-}
+  if (window.location.hash == "#conclave") {
+    fn_event_4();
+    e4.play();
+    console.log("exec");
+  }
+};
 
 callConclave();
 
-
-
-
-
-
-
 // TEST
 // document.querySelector("html").addEventListener("click",(e) => {
-//     // console.log(e.view) 
+//     // console.log(e.view)
 //     console.log(e.view.outerHeight , e.view.outerWidth)
-    
+
 //     console.log(e.view.innerHeight , e.view.innerWidth)
 
-    
 // yheight = -e.view.innerHeight + 0.2 * e.view.innerHeight;
 // xwidth= -e.view.innerWidth + 0.11 * e.view.innerWidth;
 // })
 
-
 // // TEST
 // // document.querySelector("html").addEventListener("click",(e) => {
-// //     // console.log(e.view) 
+// //     // console.log(e.view)
 // //     console.log(e.view.outerHeight , e.view.outerWidth)
-    
+
 // //     console.log(e.view.innerHeight , e.view.innerWidth)
 
-    
 // // yheight = -e.view.innerHeight + 0.2 * e.view.innerHeight;
 // // xwidth= -e.view.innerWidth + 0.11 * e.view.innerWidth;
 // // })
-
-
-
