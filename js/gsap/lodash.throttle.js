9d4c7824b604e664886ca0901ec9e25b94e4d421
KarthikS373 <lit2021012@iiitl.ac.in>
2023-01-10 14:29:06 +0530

    Technical timeline

---
+++ js/gsap/lodash.throttle.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - lodash.throttle@4.1.1
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/lodash.throttle
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/lodash.throttle@v4.1.1-F50y3ZtJgnO9CirUfqrt/mode=imports/optimized/lodash.throttle.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/lodash.throttle@v4.1.1-F50y3ZtJgnO9CirUfqrt/mode=imports,min/optimized/lodash.throttle.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/lodash.throttle@v4.1.1-F50y3ZtJgnO9CirUfqrt/dist=es2019,mode=imports/optimized/lodash.throttle.js';
+export {default} from '/-/lodash.throttle@v4.1.1-F50y3ZtJgnO9CirUfqrt/dist=es2019,mode=imports/optimized/lodash.throttle.js';
